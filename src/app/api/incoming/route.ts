8a556ea4f16a4681f101598f9ea2290cab876434
Kelson Warner <kelson.warner@amplitude.com>
2023-05-10 18:53:08 -0700

    add incoming

--- src/app/api/incoming/route.ts
+++ src/app/api/incoming/route.ts
@@ -7,3 +7,11 @@ export async function GET(request: Request) {
 
   return NextResponse.json({ data });
 }
+
+export async function POST(request: Request) {
+  const data = await request.json();
+
+  console.log({ data, h: 1 });
+
+  return NextResponse.json({ data });
+}
