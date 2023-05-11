ea364efecbd63a1460a860320f01bcc8ee87e8db
Kelson Warner <kelson.warner@amplitude.com>
2023-05-10 19:38:10 -0700

    remove get

--- src/app/api/incoming/route.ts
+++ src/app/api/incoming/route.ts
@@ -1,8 +1,7 @@
 export async function POST(request: Request) {
   const data = await request.json();
 
-  const message = JSON.parse(data.Message);
-  const email = Buffer.from(message.data, "base64").toString("utf8");
+  const email = Buffer.from(data.Message, "base64").toString("utf8");
   console.log(email);
   // At this point, email is the raw content of the email
   // You can parse and store this data in the database and associate with the user
