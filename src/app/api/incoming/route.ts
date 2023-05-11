b20103ba6d2b8e2f403aa460a61965c93f44e09f
Kelson Warner <kelson.warner@amplitude.com>
2023-05-10 19:32:22 -0700

    remove get

--- src/app/api/incoming/route.ts
+++ src/app/api/incoming/route.ts
@@ -1,11 +1,13 @@
 export async function POST(request: Request) {
   const data = await request.json();
 
-  const url = data.SubscribeURL;
-  console.log({ url });
-  await fetch(url);
+  const message = JSON.parse(data.Message);
+  const email = Buffer.from(message.data, "base64").toString("utf8");
+  console.log(email);
+  // At this point, email is the raw content of the email
+  // You can parse and store this data in the database and associate with the user
 
-  return new Response(url, {
+  return new Response(email, {
     status: 200,
     headers: {
       "Access-Control-Allow-Origin": "*",
