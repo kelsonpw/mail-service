96b81a186c02a35b14371ae9d225cfa5f6305d9c
Kelson Warner <kelson.warner@amplitude.com>
2023-05-10 19:10:54 -0700

    remove get

--- src/app/api/incoming/route.ts
+++ src/app/api/incoming/route.ts
@@ -1,14 +1,5 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const data = await request.json();
-  const url = data.SubscribeURL;
-  console.log({ url });
-  await fetch(url);
-
-  return NextResponse.json({ data: url });
-}
-
 export async function POST(request: Request) {
   const data = await request.json();
 
